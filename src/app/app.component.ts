a3bc6c7f0ddbbad9bf63e7e5fa687ecc8ff29ca7
Ugur Gül <ugurgul9@gmail.com>
2025-02-21 14:31:03 +0100

    feature: prime-ng is added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+import { ToggleSwitch } from 'primeng/toggleswitch';
+import { PrimeIcons, MenuItem } from 'primeng/api';
+import { RadioButtonModule } from 'primeng/radiobutton';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, FormsModule, ButtonModule, ToggleSwitch, RadioButtonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'best-practice-angular';
+  items: MenuItem[] = [];
+  ingredient!: string;
+  ngOnInit() {
+    this.items = [
+      {
+        label: 'New',
+        icon: PrimeIcons.PLUS,
+      },
+      {
+        label: 'Delete',
+        icon: PrimeIcons.TRASH,
+      },
+    ];
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,8 +2,84 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import {
+  provideClientHydration,
+  withEventReplay,
+} from '@angular/platform-browser';
+
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeng/themes/aura';
+import { definePreset } from '@primeng/themes';
+
+const Noir = definePreset(Aura, {
+  semantic: {
+      primary: {
+          50: '{zinc.50}',
+          100: '{zinc.100}',
+          200: '{zinc.200}',
+          300: '{zinc.300}',
+          400: '{zinc.400}',
+          500: '{zinc.500}',
+          600: '{zinc.600}',
+          700: '{zinc.700}',
+          800: '{zinc.800}',
+          900: '{zinc.900}',
+          950: '{zinc.950}'
+      },
+      colorScheme: {
+          light: {
+              primary: {
+                  color: '{zinc.950}',
+                  inverseColor: '#ffffff',
+                  hoverColor: '{zinc.900}',
+                  activeColor: '{zinc.800}'
+              },
+              highlight: {
+                  background: '{zinc.950}',
+                  focusBackground: '{zinc.700}',
+                  color: '#ffffff',
+                  focusColor: '#ffffff'
+              }
+          },
+          dark: {
+              primary: {
+                  color: '{zinc.50}',
+                  inverseColor: '{zinc.950}',
+                  hoverColor: '{zinc.100}',
+                  activeColor: '{zinc.200}'
+              },
+              highlight: {
+                  background: 'rgba(250, 250, 250, .16)',
+                  focusBackground: 'rgba(250, 250, 250, .24)',
+                  color: 'rgba(255,255,255,.87)',
+                  focusColor: 'rgba(255,255,255,.87)'
+              }
+          }
+      }
+  }
+});
+
+// PrimeNG Providers
+const PRIME_NG = [
+  provideAnimationsAsync(),
+  providePrimeNG({
+    theme: {
+      preset: Noir,
+
+      options: {
+        darkModeSelector: false,
+      },
+    },
+  }),
+];
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay())]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideClientHydration(withEventReplay()),
+    ...PRIME_NG,
+  ],
 };
+
